c20267dbf3f2604d094d168691fac286ee5341fd
‘Mayuri’ <mcandagaddala@oakland.edu>
2023-01-24 22:31:06 -0500

    navbar and footer

--- bldit-web/src/components/Navbar.js
+++ bldit-web/src/components/Navbar.js
@@ -1,52 +1,36 @@
 import React, { useState } from "react";
-import {
-  NavbarContainer,
-  LeftContainer,
-  RightContainer,
-  NavbarExtendedContainer,
-  NavbarInnerContainer,
-  NavbarLinkContainer,
-  NavbarLink,
-  Logo,
-  OpenLinksButton,
-  NavbarLinkExtended,
-} from "../styles/Navbar.styles.js";
-import LogoImg from "../assets/AM logo.jpeg";
+import Logo from "../assets/AM-logo.jpeg";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import "../styles/Navbar.css";
 
 function Navbar() {
-  const [extendNavbar, setExtendNavbar] = useState(false);
+  const [openLinks, setOpenLinks] = useState(false);
 
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
   return (
-    <NavbarContainer extendNavbar={extendNavbar}>
-      <NavbarInnerContainer>
-        <LeftContainer>
-          <NavbarLinkContainer>
-            <NavbarLink to="/"> Home</NavbarLink>
-            <NavbarLink to="/projects"> Projects</NavbarLink>
-            <NavbarLink to="/sign in"> Sign In</NavbarLink>
-            <NavbarLink to="/sign up"> Sign Up</NavbarLink>
-            <OpenLinksButton
-              onClick={() => {
-                setExtendNavbar((curr) => !curr);
-              }}
-            >
-              {extendNavbar ? <>&#10005;</> : <> &#8801;</>}
-            </OpenLinksButton>
-          </NavbarLinkContainer>
-        </LeftContainer>
-        <RightContainer>
-          <Logo src={LogoImg}></Logo>
-        </RightContainer>
-      </NavbarInnerContainer>
-      {extendNavbar && (
-        <NavbarExtendedContainer>
-          <NavbarLinkExtended to="/"> Home</NavbarLinkExtended>
-          <NavbarLinkExtended to="/projects"> Projects</NavbarLinkExtended>
-          <NavbarLinkExtended to="/sign in"> Sign In</NavbarLinkExtended>
-          <NavbarLinkExtended to="/sign up"> Sign Up</NavbarLinkExtended>
-        </NavbarExtendedContainer>
-      )}
-    </NavbarContainer>
+    <div className="navbar">
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <img alt ="logo" src={Logo} />
+        <div className="hiddenLinks">
+          <Link to="/"> Home </Link>
+          <Link to="/Projects"> Projects </Link>
+          <Link to="/Sign In"> Sign In </Link>
+          <Link to="/Sign Up"> Sign Up </Link>
+        </div>
+      </div>
+      <div className="rightSide">
+        <Link to="/"> Home </Link>
+        <Link to="/Projects"> Projects </Link>
+        <Link to="/Sign In"> Sign In </Link>
+        <Link to="/Sign Up"> Sign up </Link>
+        <button onClick={toggleNavbar}>
+          { <FaBars />}
+        </button>
+      </div>
+    </div>
   );
 }
 
--- bldit-web/src/pages/Home.js
+++ bldit-web/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BannerImage from "../assets/AM logo.jpeg";
+import BannerImage from "../assets/AMbg.jpeg";
 import "../styles/Home.css";
 
 function Home() {
@@ -8,6 +8,7 @@ function Home() {
     <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
       <div className="headerContainer">
         <h1> Auto Mates </h1>
+        <p> System that allows developers to automate their tasks.</p>
         <Link to="/Projects">
           <button> Projects </button>
         </Link>

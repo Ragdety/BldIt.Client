abb35eb0a9f2f664453face1b67cc3b67e650b99
kayabegum123 <91554169+kayabegum123@users.noreply.github.com>
2023-02-03 10:30:52 -0500

    Registration

--- bldit-web/src/pages/Login.js
+++ bldit-web/src/pages/Login.js
@@ -13,47 +13,77 @@ import ForgotPassword from "./ForgotPassword";
 import Home from "./Home";
 
 function LoginForm() {
-  // React States
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [usernameOrEmail, setUsernameOrEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [error, setError] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [token, setToken] = useState("");
 
-  // User Login info
-  const database = [
-    {
-      username: "user1",
-      password: "pass1",
-    },
-    {
-      username: "user2",
-      password: "pass2",
-    },
-  ];
+  // // React States
+  // const [isSubmitted, setIsSubmitted] = useState(false);
 
-  const errors = {
-    uname: "invalid username",
-    pass: "invalid password",
+  const login = async () => {
+    //We're calling the backend api here
+    await BlditApi.post("/identity/login", {
+      //This is the data we want to send (username and password)
+      //which come from the react states
+      UserNameOrEmail: usernameOrEmail,
+      Password: password,
+    })
+      .then((response) => {
+        //"THEN" once we send the request, we get a response
+
+        //With some data in it:
+        const data = response.data;
+        console.log(data);
+
+        //We set the errors to false since we successfully logged in
+        setError(false);
+        setErrors([]);
+        setIsSuccess(true);
+
+        //And we set the jwt token (which contains the user information)
+        setToken(data.token);
+        console.log(token);
+      })
+      .catch((e) => {
+        //This point is reached if the api returned a 4xx response (meaning there was an error/problem)
+
+        //The data is the JSON payload the api returns (which contains the errors)
+        const data = e.response.data;
+        console.log(data);
+
+        //These are the errors given by the API which we set to the errors state so we can render them out
+        setErrors(data.Errors);
+        setError(true);
+      });
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     //Prevent page reload
     event.preventDefault();
+    //navigate("/home");
+    await login();
+  };
 
-    var { uname, pass } = document.forms[0];
+  const handleChange = (event, name) => {
+    //Define the login user data
+    const user = {};
 
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
+    //This comes from the form input values
+    user[name] = event.target.value;
 
-    // Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
+    //Set the appropriate fields and states
+    switch (name) {
+      case "usernameOrEmail":
+        setUsernameOrEmail(user.usernameOrEmail);
+        break;
+      case "password":
+        setPassword(user.password);
+        break;
+      default:
+        break;
     }
   };
 
@@ -88,14 +118,24 @@ function LoginForm() {
       <img src={logo} className="logo" id="logo" alt="Built it logo" />
       <form onSubmit={handleSubmit}>
         <div className="input-container">
-          <label>Username </label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
+          <label>Username Or Email</label>
+          <input
+            type="text"
+            name="uname"
+            required
+            onChange={(e) => handleChange(e, "usernameOrEmail")}
+          />
+          {/* {renderErrorMessage("uname")} */}
         </div>
         <div className="input-container">
           <label>Password </label>
-          <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
+          <input
+            type="password"
+            name="pass"
+            required
+            onChange={(e) => handleChange(e, "password")}
+          />
+          {/* {renderErrorMessage("pass")} */}
         </div>
 
         <div className="input-container">
@@ -112,22 +152,19 @@ function LoginForm() {
           Forgot Password
         </button>
       </div>
+      {/*We render the errors here (if any)*/}
+      {error && (
+        <p style={{ color: "red", marginTop: 5 }}>
+          {errors.map((error) => error)}
+        </p>
+      )}
     </div>
   );
 
   return (
     <div className="app">
       <div className="login-form">
-        {/* <img src={logo} className="logo" id="logo" alt="Built it logo" /> */}
-        {/* {isSubmitted ? <div>User is successfully logged in</div> : renderForm} */}
-        {/*{isSubmitted ? <Navigate to="/home" /> : renderForm}*/}
-        {renderForm}
-        {/* define routes */}
-        <Routes>
-          <Route exact path="/signup" element={<SignUp />} />
-          <Route exact path="/forgotpassword" element={<ForgotPassword />} />
-          <Route exact path="/home" element={<Home />} />
-        </Routes>
+        {isSuccess ? <div>User is successfully logged in</div> : renderForm}
       </div>
     </div>
   );

2dd452c46ae805ecc3e2e79b433f5e1f01e046f5
kayabegum123 <91554169+kayabegum123@users.noreply.github.com>
2023-02-03 10:58:20 -0500

    signup

--- bldit-web/src/pages/SignUp.js
+++ bldit-web/src/pages/SignUp.js
@@ -152,7 +152,10 @@ function RegistrationForm() {
 
   return (
     <div className="app">
+<<<<<<< Updated upstream
       //rendering form
+=======
+>>>>>>> Stashed changes
       <div className="login-form">{renderForm}</div>
     </div>
   );

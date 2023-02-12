59e9a1f6e837c1a268c07c445dbba24efb479a29
‘Mayuri’ <mcandagaddala@oakland.edu>
2023-02-12 17:36:17 +0530

    Homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import Logo from "../assets/AM-logo.jpeg";
+import { useNavigate } from "react-router-dom";
+import "./ContactForm.js";
+import "../styles/Footer.css";
 
 function Footer() {
+
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
+  const navigate = useNavigate();
+
+  const navigateToContactForm = () => {
+    //  navigate to /contactform
+    navigate("/contactform");
+  };
+
   return (
     <div className="footer">
-      <p> &copy; 2023 Auto Mates</p>
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <img alt="logo" src={Logo} />
+        <p> &copy; 2023 Auto Mates</p>
+        <hr/>
+        <div className="contactform">
+          <button type="click" onClick={navigateToContactForm}>
+            Contact Me
+          </button>
+        </div>
+      </div>
     </div>
   );
 }

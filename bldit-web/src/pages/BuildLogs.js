ce62fb26e58ec9b3b926ff3de66b44dceb0c9bad
Edgar Terrazas <ragdeterra@gmail.com>
2023-03-06 23:49:02 -0500

    Added real-time logs card

--- bldit-web/src/pages/BuildLogs.js
+++ bldit-web/src/pages/BuildLogs.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as signalR from "@microsoft/signalr";
 import axios from "axios";
+import Navbar from "../components/Navbar";
+import BannerImage from "../assets/pic.png";
+import Footer from "../components/Footer";
 
 const BuildLogs = () => {
   const buildStreamURL = 'http://localhost:5005/buildStream';
   const buildURL = "http://localhost:5003/api/v1";
 
   const [currentRoom, setCurrentRoom] = useState("");
-  const [runningBuildId, setRunningBuildId] = useState("");
+  const [runningBuild, setRunningBuild] = useState({});
+  const [logs, setLogs] = useState([]);
+
+  const logsRef = useRef(null)
+  
+  const scrollToBottom = () => {
+    logsRef.current && logsRef.current.scrollIntoView({ behavior: "smooth" })
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [logs]);
 
   const connection = new signalR.HubConnectionBuilder()
     .withUrl(buildStreamURL, {
@@ -38,7 +52,17 @@ const BuildLogs = () => {
 
       console.log(result.logs);
       setCurrentRoom(buildLogRoom);
-      connection.on("BuildOutputReceived", log => console.log(log));
+      connection.on("BuildOutputReceived", 
+          log => {
+            console.log(log)
+            setLogs(prevState => {
+              return [...prevState, log];
+            });
+            const element = React.createElement("p", 
+              {className: "mb-3 font-normal text-gray-700 dark:text-gray-400"}, 
+              log)
+          }
+      );
     })
     .catch((e) => console.log('Connection failed: ', e));
 
@@ -67,22 +91,56 @@ const BuildLogs = () => {
 
         const createdBuild = r.data;
         console.log(createdBuild);
-        setRunningBuildId(createdBuild.id);
+        setRunningBuild(createdBuild);
       });
   }
   
   return (
-    <div>
-      <button onClick={() => {build('f0cc16a1-fdbf-462f-b4c7-e020b46237a6', 'Test5');}}>
-        Build
-      </button>
-      {/*This button simulates clicking the build log page*/}
-      <button onClick={async () => await join(runningBuildId)}>
-        Build Log
-      </button>
-      <button onClick={async () => await leave()}>
-        Leave Build Log Page
-      </button>
+    <div className="content">
+      <div className="content">
+        <Navbar />
+        <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+          <div className="flex items-center justify-center h-screen">
+            <div
+              className="max-w-sm w-full p-6 text-center bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <p>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Realtime Logs
+                </h5>
+              </p>
+              <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center 
+            text-white bg-green-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none 
+            focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      onClick={() => {build('f0cc16a1-fdbf-462f-b4c7-e020b46237a6', 'Test5');}}>
+                Build
+              </button>
+              <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center 
+             text-white bg-green-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none 
+             focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      onClick={async () => await join(runningBuild.id)}>
+                Logs
+              </button>
+              <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center 
+             text-white bg-green-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none 
+             focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      onClick={async () => await leave()}>
+                Leave
+              </button>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 max-h-screen overflow-y-auto max-h-72" >
+                Logs from build: {runningBuild.number}
+                {logs.map((log, index) => (
+                    <p key={index}
+                       ref={logsRef}
+                       className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                      {log}
+                    </p>
+                  )
+                )}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

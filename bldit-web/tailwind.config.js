2c16cc02cdd414df96f23367feb1773436126584
Edgar Terrazas <ragdeterra@gmail.com>
2023-03-06 22:30:16 -0500

    Added tailwind and some basic modifications to css

---
+++ bldit-web/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  corePlugins: {
+    preflight: false,
+  }
+}
